715894b0687909adddee24e288ed3d23e5240ac0
Sven Ziegler <info@sven-ziegler.com>
2020-06-05 14:36:30 +0200

    added prod replacement params

--- lib/fastlane/plugin/ionic/actions/ionic_action.rb
+++ lib/fastlane/plugin/ionic/actions/ionic_action.rb
@@ -93,6 +93,7 @@ module Fastlane
       # actual building! (run #2)
       def self.build(params)
         args = [params[:release] ? '--release' : '--debug']
+        args << '--uglifyjs --minifyjs --minifycss --aot' if params[:release]
         args << '--device' if params[:device]
         args << '--prod' if params[:prod]
         args << '--browserify' if params[:browserify]
